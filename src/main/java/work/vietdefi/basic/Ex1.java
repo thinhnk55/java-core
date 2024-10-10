175d565ead90aa01f6a10389a10645821dd90e95
Khanh Thinh Nguyen <khanhthinh.45a4@gmail.com>
2024-10-10 18:49:31 +0700

    feat(java-bacic): #3 Ex1

---
+++ src/main/java/work/vietdefi/basic/Ex1.java
@@ -0,0 +1,13 @@
+package work.vietdefi.basic;
+
+public class Ex1 {
+    /**
+     * Determines if the given number is even.
+     *
+     * @param input the number to check
+     * @return true if the input is even; false if the input is odd
+     */
+    public boolean isEvenNumber(int input) {
+        return input % 2 == 0; // Returns true if the number is even, false otherwise
+    }
+}
