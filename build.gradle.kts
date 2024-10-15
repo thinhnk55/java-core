4515a9937bfbb50e866d0f383608aea5158ca430
Khanh Thinh Nguyen <khanhthinh.45a4@gmail.com>
2024-10-15 17:13:08 +0700

    feat(java-json): #5 GsonConverter and JacksonConverter

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.16")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-log4j12
     implementation("org.slf4j:slf4j-log4j12:2.0.16")
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
